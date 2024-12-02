a7c605c54d55be679362239a00b57531beeff09f
Matthew D <mattdeltano@gmail.com>
2024-12-01 19:13:32 -0500

    exit logic

--- shell.c
+++ shell.c
@@ -4,13 +4,14 @@
 #define BUFFER_LEN 256 //length of buffer where shell commands are stored
 
 char ERROR_MESSAGE[30] = "An error has occurred\n";
+int exit_switch = 0;
 
 
 
 int main(int argc, char **argv){
 do{
     print_shell();
-    parse();
+    exit_switch = parse();
 
 
 
@@ -19,7 +20,7 @@ do{
 
 
 
-} while(1);
+} while(exit_switch == 0);
 }
 
 
@@ -29,30 +30,34 @@ void print_shell(){
 
 int parse(){
     char buffer[BUFFER_LEN];
-    char cmd[BUFFER_LEN];
-    char* args[BUFFER_LEN];
-    int i = 0;
-    int j = 0;
     
     fgets(buffer, BUFFER_LEN, stdin);
-    tokenize(&buffer);
+    char* tokens[BUFFER_LEN][BUFFER_LEN] = tokenize(&buffer);
 
-    
+    if(strcmp(tokens[0][BUFFER_LEN], "exit") == 0 && strlen(tokens[1][BUFFER_LEN] != 0)){
+        write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+    }
+    if(strcmp(tokens[0][BUFFER_LEN], "exit") == 0){
+        return 1;
+    }
+
+
 }
 
 char* tokenize(char* buffer){
     int i = 0;
     int j = 0;
-    char* tokens[BUFFER_LEN][BUFFER_LEN];
+    char* tokens[BUFFER_LEN][BUFFER_LEN] = NULL;
 
     for(int j; j < BUFFER_LEN; j++){
-        if(buffer[j-1] == (" " || "\t") && buffer[j] != (" " || "\t")){
+        if(buffer[j-1] == (' ' || '\t') && buffer[j] != (' ' || '\t')){
+            tokens[i][j] = '\0';
             i++;
         }  
-        if(buffer[j] != (" " || "\t")){
+        if(buffer[j] != (' ' || '\t')){
             tokens[i][j] = buffer[j];
         }
     }
 
-
+    return tokens;
 }
